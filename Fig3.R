e9be55301749e698dd9dd4c24b6495321fb12af2
salahuddin975 <salahuddin975@gmail.com>
2019-02-15 13:03:00 -0600

    Add Vertical line for mean.

--- Fig3.R
+++ Fig3.R
@@ -1,23 +1,32 @@
-# Set working directory
 setwd("~/Workspace/RStudio/")
+#setwd("H:/Ph.D/2nd Semester/Statistical Method in Research/Project/sm-xd-evolution")
 
-# Read fgsfd csv file
 library(readr)
 fgsfd = read.csv("data/Faculty_GoogleScholar_Funding_Data_N4190.csv")
+
 #head(fgsfd); nrow(fgsfd)
+
 library(dplyr)
 df = data.frame(fgsfd %>% select(XDIndicator, min_year, KTotal, Chi, mean_of_IF))
 
 # Fig3-A: Probability distribution of the year of first publication
+
 library(ggplot2)
-ggplot(df, aes(x = min_year, color = XDIndicator, fill = XDIndicator)) + 
-  geom_density(alpha = 0.5) +
+
+fig_3_a = ggplot(df, aes(x = min_year, color = XDIndicator, fill = XDIndicator)) + 
+  geom_density(alpha = 0.5)
+
+mu <- ddply(df, "XDIndicator", summarise, grp.mean=mean(min_year))
+
+fig_3_a + geom_vline(data=mu, aes(xintercept=grp.mean, color=XDIndicator),
+                     linetype="dashed") +
   xlab(expression(paste("Year of first publication, ",y[i]^0,sep=''))) + 
   ylab(expression(paste("PDF(",y[i]^0,")",sep=''))) +   
   scale_x_continuous(expand = c(0, 0), 
                      limits = c(1955, max(df$min_year)+3), 
-                     breaks = seq(1960, 2010, 10)) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0.00, 0.04)) +   
+                     breaks = c(1960, 1970, 1980, 1990, 2000, 2010)) +
+  scale_y_continuous(expand = c(0, 0), 
+                     limits = c(0.00, 0.04)) +   
   theme(panel.background = element_blank(), 
         panel.border = element_rect(color = "black", fill = NA))
 
