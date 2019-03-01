0fe2f5c782973ac98cef2f3c03ebfcd8fec3d413
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-03-01 00:20:40 -0600

    FigS2 (ylog10)

--- FigS2.R
+++ FigS2.R
@@ -12,10 +12,10 @@ df = df %>% select(dept, KTotal, KDirect, KMediated, XDIndicator)
 # FigS2: a. biology
 df0 = filter(df, df$dept == "BIO")
 hg0 = ggplot(data=df0) +
-  geom_histogram(aes(x=df0$KMediated),
+  geom_histogram(aes(x=df0$KMediated,y=..count..+1),
                  breaks=seq(0,1000,20),
                  col="red", fill="red", alpha=0.3) +
-  geom_histogram(aes(x=df0$KDirect),
+  geom_histogram(aes(x=df0$KDirect,y=..count..+1),
                  breaks=seq(0,1000,2),
                  col="blue", fill="blue", alpha=0.3) +
   xlab(expression(paste(d[i],", link degree", sep=""))) + 
@@ -23,24 +23,22 @@ hg0 = ggplot(data=df0) +
   scale_x_continuous(expand = c(0, 0),
                      limits = c(-20, 1020),
                      breaks = seq(0, 1000, 200)) +
-  scale_y_continuous(expand = c(0, 0),
-                     limits = c(0, 1000),
-                     breaks = c(0,1,10,100, 1000)) +
+  scale_y_log10(expand = c(0, 0)) +
   annotate(geom="text", hjust=0, size=3, colour='red', 
-           x=75, y=725, label="a", face = "bold") +
+           x=75, y=275, label="a") +
   annotate(geom="text", hjust=0, size=2, colour='red', 
-           x=575, y=750, label="- Mediated (Biology)") +
+           x=500, y=375, label="- Mediated (Biology)") +
   annotate(geom="text", hjust=0, size=2, colour='blue', 
-           x=575, y=700, label="- Direct (Biology)") +
+           x=500, y=275, label="- Direct (Biology)") +
   theme(plot.title = element_text(hjust = 0.5, size = 7, face = "bold"))
 
 # FigS2: b. computing
 df1 = filter(df, df$dept == "CS")
 hg1 = ggplot(data=df1) +
-  geom_histogram(aes(x=df1$KMediated),
+  geom_histogram(aes(x=df1$KMediated,y=..count..+1),
                  breaks=seq(0,1000,20),
                  col="red", fill="red", alpha=0.3) +
-  geom_histogram(aes(x=df1$KDirect),
+  geom_histogram(aes(x=df1$KDirect,y=..count..+1),
                  breaks=seq(0,1000,2),
                  col="blue", fill="blue", alpha=0.3) +
   xlab(expression(paste("", d[i],", link degree", sep=""))) + 
@@ -48,15 +46,13 @@ hg1 = ggplot(data=df1) +
   scale_x_continuous(expand = c(0, 0),
                      limits = c(-20, 1020),
                      breaks = seq(0, 1000, 200)) +
-  scale_y_continuous(expand = c(0, 0),
-                     limits = c(0, 500),
-                     breaks = c(0,1,5,10,50,100,500)) +
+  scale_y_log10(expand = c(0, 0)) +
   annotate(geom="text", hjust=0, size=3, colour='red', 
-           x=75, y=450, label="b", face = "bold") +
+           x=75, y=175, label="b") +
   annotate(geom="text", hjust=0, size=2, colour='red', 
-           x=575, y=470, label="- Mediated (Computing)") +
+           x=500, y=175, label="- Mediated (Computing)") +
   annotate(geom="text", hjust=0, size=2, colour='blue', 
-           x=575, y=425, label="- Direct (Computing)") +
+           x=500, y=125, label="- Direct (Computing)") +
   theme(plot.title = element_text(hjust = 0.5, size = 7, face = "bold"))
 
 # Show Histogram
