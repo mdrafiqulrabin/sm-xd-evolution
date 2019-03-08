370aad68089657632cf3a34730b1546fde3ad674
salahuddin975 <salahuddin975@gmail.com>
2019-03-08 14:49:38 -0600

    Fig S1 code

--- FigS1.R
+++ FigS1.R
@@ -9,13 +9,14 @@ gra_s1 <- graph.edgelist(data_matrix, directed=FALSE)
 
 s1_f4 <- swan_combinatory(gra_s1,40)
 
-plot(s1_f4[,1],s1_f4[,5], type='o', col='yellow',xlab="q, fraction of removed links", ylab="Connectivity loss")
-lines(s1_f4[,1],s1_f4[,3], type='o', col='red')
-lines(s1_f4[,1],s1_f4[,4], type='o', col='orange')
-lines(s1_f4[,1],s1_f4[,2], type='o', col='blue')
-legend('bottomright',c("Random", "Betweenness", "Degree", "Cascading"), 
+s1_f4 <- read.csv("result_i10.csv")
+
+plot(1 - s1_f4[,1],s1_f4[,5], type='o', col='yellow',xlab="q, fraction of removed links", ylab="Connectivity loss")
+lines(1 - s1_f4[,1],s1_f4[,3], type='o', col='red')
+lines(1 - s1_f4[,1],s1_f4[,4], type='o', col='orange')
+lines(1 - s1_f4[,1],s1_f4[,2], type='o', col='blue')
+legend('bottomleft',c("Random", "Betweenness", "Degree", "Cascading"), 
        lty=c(1,1,1,1), pch=c(1,1,1,1), 
        col=c("yellow","blue","red", "orange"))
-
 
 
