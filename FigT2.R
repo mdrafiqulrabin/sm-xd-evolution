e0a14ecfbbf63e931ab9650383222911987c29ea
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-03-29 14:49:11 -0500

    FigT2: CV network

--- FigT2.R
+++ FigT2.R
@@ -6,15 +6,18 @@ library(dplyr, warn.conflicts=F)
 library(readr, warn.conflicts=F)
 
 # Read Faculty_GoogleScholar_Funding_Data_N4190.csv
-# google_id                    name dept h_index i10index min_year max_year t_publication t_pubs_citations
+# [google_id name dept h_index i10index min_year max_year t_publication t_pubs_citations
 # highest_citations mean_of_IF mean_of_co_authors num_nsf t_deflated_nsf num_nih t_deflated_nih Y05yr KTotal
-# KDirect KMediated        Chi BetCentrality PRCentrality XDIndicator SchoolRank
+# KDirect KMediated Chi BetCentrality PRCentrality XDIndicator SchoolRank]
 df = read.csv("Data/Faculty_GoogleScholar_Funding_Data_N4190.csv")
 
 cv = df %>% select(t_pubs_citations, SchoolRank, h_index, 
                    t_deflated_nsf, num_nsf, t_deflated_nih, num_nih, 
                    XDIndicator, Y05yr)
-cv[,1:7] = log(cv[1:7]+0.1)
+cv[,1] = log(cv[1]) #min=3
+cv[,2:3] = log(cv[2:3] + 0.002) #min=1
+cv[,4:7] = log(cv[4:7] + 1) #min=0
+
 model_cv = lm (t_pubs_citations ~ SchoolRank + h_index +
                  t_deflated_nsf + num_nsf + t_deflated_nih + num_nih + 
                  factor(XDIndicator) + factor(Y05yr), data=cv)
