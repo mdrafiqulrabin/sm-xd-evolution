760fe645a9d431bb21fd92224e04aa4f32f66dc3
salahuddin975 <salahuddin975@gmail.com>
2019-03-30 12:06:48 -0500

    Add margin.

--- Fig4.R
+++ Fig4.R
@@ -13,15 +13,16 @@ df <- data.frame(Parameters = c('Br', 'B$1', 'Bn1', 'B$2', 'Bn2', 'BcPR', 'Bx'),
                  Error = c(0.006, 0.02, 0.015, 0.014, 0.014, 0.012, 0.011))
 
   
-  fig4 <- ggplot(df, aes(x=Parameters, y=Standardized))
-  fig4 + 
-    geom_point(size = 2) +
-    geom_errorbar( aes(ymin = Error + Standardized, ymax = Standardized - Error), 
-                   width=0.2) +
+    fig4 <- ggplot(df, aes(x=Parameters, y=Standardized))
+
+    fig4 + geom_point(size = 2) +
+    geom_errorbar( aes(ymin = Error + Standardized, 
+                       ymax = Standardized - Error), width=0.2) +
     labs(x = "", y = "   Standardized regression coefficients point 
          estimate with 95% confidence interval") +
     aes(x = fct_inorder(Parameters)) +
-    coord_cartesian(ylim = c(-.10, .10), clip = 'off')
+    coord_cartesian(ylim = c(-.10, .10), clip = 'off') +
+    theme(plot.margin = unit(c(5,3,5,3), "lines"))
     
     
     
