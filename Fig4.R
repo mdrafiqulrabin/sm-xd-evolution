99fa9b3072cb873d7dc53cf22b9911692cc53f09
salahuddin975 <salahuddin975@gmail.com>
2019-03-30 22:51:02 -0500

    Change background.

--- Fig4.R
+++ Fig4.R
@@ -50,7 +50,12 @@ df <- data.frame(Parameters = c('\u03B2r', '\u03B2$1', '\u03B2n1', '\u03B2$2', '
       
       geom_text(aes(x=7.0,y=0.13,label="*")) +
       geom_text(aes(x=7.0,y=0.122,label="*")) +
-      geom_text(aes(x=7.0,y=0.114,label="*")) 
+      geom_text(aes(x=7.0,y=0.114,label="*")) +
+      
+      theme(axis.line = element_line(colour = "black"),
+            panel.background = element_blank()
+      )
+    
       
     
     
