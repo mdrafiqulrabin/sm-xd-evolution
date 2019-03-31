d878adf4081a4e2e13487cab2e65f454f0568062
salahuddin975 <salahuddin975@gmail.com>
2019-03-30 23:30:29 -0500

    increase thickness of error bar.

--- Fig4.R
+++ Fig4.R
@@ -8,18 +8,17 @@ fig4_data = read.csv("Data/Fig4/fig4.csv")
 head(fig4_data)
 
 
-df <- data.frame(Parameters = c('\u03B2r', '\u03B2$1', '\u03B2n1', '\u03B2$2', '\u03B2n2', '\u03B2cPR', '\u03B2x'),
+df <- data.frame(Parameters = c('Br', 'B$1', 'Bn1', 'B$2', 'Bn2', 'BcPR', 'Bx'),
                  Standardized = c(-0.056, -0.036, 0.015, 0.082, -0.068, 0.026, 0.085),
                  Error = c(0.006, 0.02, 0.015, 0.014, 0.014, 0.012, 0.011))
 
-  
 fig4 <- ggplot(df, aes(x=Parameters, y=Standardized))
 
 fig4 + geom_point(size = 2) +
   geom_errorbar( aes(ymin = Error + Standardized, 
-                     ymax = Standardized - Error), width=0.2) +
+                     ymax = Standardized - Error), width=0.1, linetype="solid", lwd = 1) +
     aes(x = fct_inorder(Parameters)) +
-    labs(x = "", y = "   Standardized regression coefficients point 
+    labs(x = "", y = "    Standardized regression coefficients point 
        estimate with 95% confidence interval") +
     coord_cartesian(ylim = c(-.10, .10), clip = 'off') +
     theme(plot.margin = unit(c(5,3,5,3), "lines")) +
