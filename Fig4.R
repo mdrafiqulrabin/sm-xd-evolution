6469a01af121c0e97ae28c6dff46af22c5881667
salahuddin975 <salahuddin975@gmail.com>
2019-03-30 20:33:14 -0500

    Add star.

--- Fig4.R
+++ Fig4.R
@@ -34,7 +34,11 @@ df <- data.frame(Parameters = c('\u03B2r', '\u03B2$1', '\u03B2n1', '\u03B2$2', '
       geom_segment(aes(x=7.25,y=0.15,xend=7.25,yend=0.14)) +
       
       geom_text(aes(x=6.5,y=0.16,label="Network")) +
-      geom_text(aes(x=3.0,y=0.16,label="CV")) 
+      geom_text(aes(x=3.0,y=0.16,label="CV")) +
+
+      geom_text(aes(x=0.9,y=0.13,label="*")) +
+      geom_text(aes(x=0.9,y=0.122,label="*")) +
+      geom_text(aes(x=0.9,y=0.114,label="*")) 
       
     
     
