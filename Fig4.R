b8ed2ecd23264405516d8efae88f73bfa5b89dc4
salahuddin975 <salahuddin975@gmail.com>
2019-04-01 14:12:13 -0500

    Code format.

--- Fig4.R
+++ Fig4.R
@@ -2,7 +2,7 @@ library(ggplot2, warn.conflicts=F)
 library(dplyr, warn.conflicts=F)
 library(forcats, warn.conflicts=F)
 
-df <- data.frame(Parameters   = c('Br',   'B$1',  'BN1', 'B$2', 'BN2',  'BCPR', 'BX'),
+df <- data.frame(Parameters   = c('Br',   'B$1',  'BN1', 'B$2', 'BN2',  'BCPR', 'Bx'),
                  Standardized = c(-0.056, -0.036, 0.015, 0.082, -0.068, 0.026,  0.085),
                  Error        = c(0.006,  0.020,  0.015, 0.014, 0.014,  0.012,  0.011))
 
@@ -11,7 +11,8 @@ fig4 <- ggplot(df, aes(x=Parameters, y=Standardized)) +
   geom_errorbar(aes(ymin = (Error * 2) + Standardized, ymax = Standardized - (Error * 2)), 
                 width = 0.1, linetype = "solid", lwd = 1) +
   aes(x = fct_inorder(Parameters)) +
-  labs(x = "", y = "Standardized regression coefficients \n point estimate with 95% confidence interval") +
+  labs(x = "", y = "    Standardized regression coefficients point 
+       estimate with 95% confidence interval") +
   coord_cartesian(ylim = c(-0.105, 0.105), clip = 'off') +
   theme(plot.margin = unit(c(5,3,5,3), "lines")) +
   geom_hline(yintercept=c(0,8), linetype="dashed") +
@@ -43,7 +44,7 @@ fig4 <- ggplot(df, aes(x=Parameters, y=Standardized)) +
   geom_text(aes(x=7.0, y=0.130, label="*")) +
   geom_text(aes(x=7.0, y=0.122, label="*")) +
 
-  scale_x_discrete("", labels = c(expression(beta["r"]),expression(beta["$1"]), 
+  scale_x_discrete("", labels = c(expression(beta["r"]),  expression(beta["$1"]), 
                                   expression(beta["N1"]), expression(beta["$2"]), 
                                   expression(beta["N2"]), expression(beta["C"^"PR"]), 
                                   expression(beta["X"]))) +
