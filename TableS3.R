0ad98a34b30e443a648b7701cc12511cdfca49b7
Farah Naz Chowdhury <farahprity@gmail.com>
2019-04-03 04:47:07 -0500

    add p-value in t2

--- TableS3.R
+++ TableS3.R
@@ -23,7 +23,6 @@ df[,4:7] = log1p(df[4:7]) #min=0 [t_deflated_nsf, num_nsf, t_deflated_nih, num_n
 df_a <- filter(df, df$PRCentrality > 0)
 df_a['PRCentrality'] = log(df_a['PRCentrality'])
 df_a['Chi'] = log(df_a['Chi'] + 0.63)
-
 model_a = lm (t_pubs_citations ~ 
                 SchoolRank + h_index + t_deflated_nsf + num_nsf + t_deflated_nih + num_nih +
                 PRCentrality + Chi +
@@ -35,7 +34,6 @@ nrow(df_a)
 df_b <- filter(df, df$BetCentrality > 0)
 df_b['BetCentrality'] = log(df_b['BetCentrality'])
 df_b['Chi'] = log(df_b['Chi'] + 0.63)
-
 model_b = lm (t_pubs_citations ~ 
                 SchoolRank + h_index + t_deflated_nsf + num_nsf + t_deflated_nih + num_nih +
                 BetCentrality + Chi +
