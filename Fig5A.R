fece6fba1baa4886e0c087973561240ff9114217
salahuddin975 <salahuddin975@gmail.com>
2019-04-13 17:02:08 -0500

    scaling, margin, abline

--- Fig5A.R
+++ Fig5A.R
@@ -1,6 +1,3 @@
-setwd("H:/Ph.D/2nd Semester/statistical-method-in-research/Project/sm-xd-evolution")
-
-
 library(ggplot2)
 library(dplyr)
 library(forcats)
@@ -14,8 +11,12 @@ fig5A <- ggplot(df, aes(x=parameters, y=fe_standardized))
 
 fig5A + 
   geom_point(size = 2) +
-  geom_errorbar( aes(ymin = fe_standardized + fe_stand_error, ymax = fe_standardized - fe_stand_error), 
-                 width=0.2) +
+  geom_errorbar( aes(ymin = fe_standardized + fe_stand_error, 
+                     ymax = fe_standardized - fe_stand_error), 
+                 width=0.05) +
   aes(x = fct_inorder(parameters)) +
-  labs(x = "", y = "Regression\nCoefficients") 
+  labs(x = "", y = "Regression\nCoefficients") +
+  coord_cartesian(ylim = c(-.15, .22), clip = 'off') +
+  theme(plot.margin = unit(c(5,3,4,3), "lines"))  +
+  geom_hline(yintercept=c(0,3), linetype="dashed") 
   
