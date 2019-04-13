f82a012de1cd2a9841d6eb004ff8e257338ae7cf
salahuddin975 <salahuddin975@gmail.com>
2019-04-13 18:03:45 -0500

    add value for pooled data.

--- Fig5A.R
+++ Fig5A.R
@@ -6,14 +6,32 @@ df <- data.frame(parameters = c("co-authors", "author_age", "cross-disc"),
                  fe_standardized = c(0.208, -0.0978, 0.145),
                  fe_stand_error = c(0.00365, 0.0187, 0.0235)
                  )
+pooled_df = data.frame(parameters = c("co-authors", "author_age", "cross-disc"),
+                       fe_standardized = c(0.189, -0.0564, 0.126),
+                       fe_stand_error = c(0.00479, 0.00947, 0.0341)
+                  )
+
 
 fig5A <- ggplot(df, aes(x=parameters, y=fe_standardized))
 
 fig5A + 
-  geom_point(size = 2) +
-  geom_errorbar( aes(ymin = fe_standardized + fe_stand_error, 
-                     ymax = fe_standardized - fe_stand_error), 
-                 width=0.05) +
+  geom_point(data = df, 
+#             aes(x=c(0.9, 1.9, 2.9), y=fe_standardized),
+             colour = "blue", size = 1.5) +
+  geom_errorbar(aes(ymin = fe_standardized + fe_stand_error, 
+                    ymax = fe_standardized - fe_stand_error),
+#                    x=c(0.9, 1.9, 2.9),
+                width=0.04, colour = "blue", size=0.9) +
+
+  geom_point(data = pooled_df, 
+             aes(x = c(1.2, 2.2, 3.2), y=fe_standardized),
+             colour = "black", size = 1.5) +
+  geom_errorbar(data = pooled_df, 
+                aes(x=c(1.2, 2.2, 3.2),
+                ymin = fe_standardized + fe_stand_error, 
+                ymax = fe_standardized - fe_stand_error), 
+                width=0.04, colour = "black", size=0.9) +
+  
   aes(x = fct_inorder(parameters)) +
   labs(x = "", y = "Regression\nCoefficients") +
   coord_cartesian(ylim = c(-.15, .22), clip = 'off') +
