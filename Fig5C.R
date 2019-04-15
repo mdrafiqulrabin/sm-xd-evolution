69d9439dcc162069e8eaafbad68ec78913fd5095
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-15 02:49:48 -0500

    Fig5C: show.legend = F

--- Fig5C.R
+++ Fig5C.R
@@ -16,7 +16,7 @@ df = data.frame(x_temp,y_temp)
 fig5C <- ggplot(df, aes(x=x_temp, y=y_temp)) +
   geom_vline(xintercept=c(0.0,0.10), linetype="dashed") +
   geom_vline(data = df_vline, aes(xintercept = xval, linetype = df_vline$type), 
-             color = df_vline$color, lwd = 1) +
+             color = df_vline$color, lwd = 1, show.legend = F) +
   scale_x_continuous(expand = c(-0.10, 0), breaks = seq(-0.05,0.20,0.05)) +
   scale_y_continuous(expand = c(0, 0), breaks = seq(0.0,0.12,0.04)) +
   coord_cartesian(xlim = c(-0.10, 0.25), ylim = c(0.0, 0.15), clip = 'off') +
