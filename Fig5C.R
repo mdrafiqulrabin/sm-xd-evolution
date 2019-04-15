0f99be2684d9f614f0e16278cf9db63e877aecfc
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-15 02:15:46 -0500

    Fig5C: x-label and y-label

--- Fig5C.R
+++ Fig5C.R
@@ -1,5 +1,4 @@
-library(ggplot2)
-library(dplyr)
+library(ggplot2, warn.conflicts=F)
 
 x_temp = seq(0.0,0.2,0.01)
 y_temp = seq(0.0,0.2,0.01)
@@ -8,6 +7,8 @@ fig5C <- ggplot(df, aes(x=x_temp, y=y_temp)) +
   scale_x_continuous(expand = c(-0.10, 0), breaks = seq(-0.05,0.20,0.05)) +
   scale_y_continuous(expand = c(0, 0), breaks = seq(0.0,0.12,0.04)) +
   coord_cartesian(xlim = c(-0.10, 0.25), ylim = c(0.0, 0.15), clip = 'off') +
+  xlab(expression("Cross-disciplinarity coefficient,"~beta[italic("I")]~"")) +
+  ylab(expression("Prob. dist. P("~beta[italic("I")]~")")) +
   theme(axis.line = element_line(colour = "black"),
         panel.background = element_blank(),
         panel.border = element_rect(colour = "black", fill=NA, size=.5),
