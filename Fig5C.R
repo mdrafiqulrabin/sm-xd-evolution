0c8e054e3a9104c0c3940a6d46b738fe942aa02c
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-15 02:47:48 -0500

    Fig5C: beta[I] v-line

--- Fig5C.R
+++ Fig5C.R
@@ -1,5 +1,12 @@
 library(ggplot2, warn.conflicts=F)
 
+# beta[I] vline
+df_vline <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames(df_vline) <- c("xval","color","type")
+df_vline <- rbind(df_vline, data.frame(xval=0.10, color="blue" , type="line"))
+df_vline <- rbind(df_vline, data.frame(xval=0.145, color="blue" , type="dashed"))
+df_vline <- rbind(df_vline, data.frame(xval=0.19, color="blue" , type="line"))
+
 # Dummy data
 x_temp = seq(0.0,0.2,0.01)
 y_temp = seq(0.0,0.2,0.01)
@@ -8,6 +15,8 @@ df = data.frame(x_temp,y_temp)
 # Plot 5C
 fig5C <- ggplot(df, aes(x=x_temp, y=y_temp)) +
   geom_vline(xintercept=c(0.0,0.10), linetype="dashed") +
+  geom_vline(data = df_vline, aes(xintercept = xval, linetype = df_vline$type), 
+             color = df_vline$color, lwd = 1) +
   scale_x_continuous(expand = c(-0.10, 0), breaks = seq(-0.05,0.20,0.05)) +
   scale_y_continuous(expand = c(0, 0), breaks = seq(0.0,0.12,0.04)) +
   coord_cartesian(xlim = c(-0.10, 0.25), ylim = c(0.0, 0.15), clip = 'off') +
