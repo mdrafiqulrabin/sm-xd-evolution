113ad146dcc3cba88da2db9ebe5471b9a910ce30
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-15 02:47:08 -0500

    Fig5C: base vline at 0,0

--- Fig5C.R
+++ Fig5C.R
@@ -1,9 +1,13 @@
 library(ggplot2, warn.conflicts=F)
 
+# Dummy data
 x_temp = seq(0.0,0.2,0.01)
 y_temp = seq(0.0,0.2,0.01)
 df = data.frame(x_temp,y_temp)
+
+# Plot 5C
 fig5C <- ggplot(df, aes(x=x_temp, y=y_temp)) +
+  geom_vline(xintercept=c(0.0,0.10), linetype="dashed") +
   scale_x_continuous(expand = c(-0.10, 0), breaks = seq(-0.05,0.20,0.05)) +
   scale_y_continuous(expand = c(0, 0), breaks = seq(0.0,0.12,0.04)) +
   coord_cartesian(xlim = c(-0.10, 0.25), ylim = c(0.0, 0.15), clip = 'off') +
