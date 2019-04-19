9f3ed8c54cc9e140a35d578721897845c80c9aa9
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-18 22:09:30 -0500

    TableS4: f_remove_pollinators.

--- TableS4.R
+++ TableS4.R
@@ -6,4 +6,15 @@ library(readr, warn.conflicts=F)
 
 # Load data
 df_ps = read.csv("Data/GoogleScholar_paper_stats.csv")
+
+# Methods
+f_remove_pollinators <- function (x) {
+  x = unlist(strsplit(as.character(x), ",")) # unlist
+  x = x [! x %in% c(0:2)] # Remove pollinators
+  x = paste(as.character(x), collapse=",") # relist
+  return(x)
+}
+
+# Main
+df_ps$coauthor_codes = lapply(df_ps$coauthor_codes, f_remove_pollinators)
 df_ps
