8e39a27013a0ad393acfe390eebf4e63862e3061
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-20 15:30:43 -0500

    f_get_iXDp

--- Data/Panel_Analysis/Panel_Analysis.R
+++ Data/Panel_Analysis/Panel_Analysis.R
@@ -1,5 +1,5 @@
 # Set working directory
-setwd("~/Workspace/RStudio/sm-xd-evolution/Data/TableS4S5/")
+setwd("~/Workspace/RStudio/sm-xd-evolution/Data/Panel_Analysis/")
 
 # Import library
 library(readr, warn.conflicts=F)
@@ -12,6 +12,18 @@ df_cs = read.csv("../ComputerScience_citations_stats_CitationNormalizationData.c
 df = df_ps[1:100,] # working df
 
 # Methods
+f_remove_pollinators <- function (coauth) {
+  coauth = unlist(strsplit(as.character(coauth), ",")) # unlist
+  coauth = coauth [! coauth %in% c(0:2)] # Remove pollinators
+  coauth = paste(as.character(coauth), collapse=",") # relist
+  return(as.character(coauth))
+}
+
+f_get_xdf <- function (gsid) {
+  xd = (df_gs %>% filter(google_id==gsid))$XDIndicator
+  return(as.character(xd))
+}
+
 f_get_normcite <- function (gsid, pyr, ncite) {
   df_ts = ""
   if(f_get_dept(gsid) == "BIO") {
@@ -32,40 +44,27 @@ f_get_totalcoauth <- function (coauth) {
   return(n)
 }
 
-f_get_careerage <- function (gsid, pyr) {
-  myr = (df_gs %>% filter(google_id==gsid))$min_year
-  return(as.integer(pyr) - as.integer(myr) + 1)
-}
-
-f_get_bridgefrac <- function(coauth) {
-  coauth = unlist(strsplit(as.character(coauth), ","))
-  poli = coauth [coauth %in% c(0:2)]
-  frac = length(which(poli==2))/length(poli)
-  return(frac)
-}
-
-f_get_pagerank <- function(gsid) {
-  return((df_gs %>% filter(google_id==gsid))$PRCentrality)
-}
-
-f_remove_pollinators <- function (coauth) {
-  coauth = unlist(strsplit(as.character(coauth), ",")) # unlist
-  coauth = coauth [! coauth %in% c(0:2)] # Remove pollinators
-  coauth = paste(as.character(coauth), collapse=",") # relist
-  return(as.character(coauth))
+f_get_careerage <- function (gsid, pubyr) {
+  minyr = (df_gs %>% filter(google_id==gsid))$min_year
+  return(as.integer(pubyr) - as.integer(minyr) + 1)
 }
 
 f_get_dept <- function (gsid) {
-  dept0 = (df_gs %>% filter(google_id==gsid))$dept
-  return(as.character(dept0))
+  if (gsid %in% c(0,1,2)) { #pollinators
+    return(as.integer(gsid))
+  } else { #faculty
+    dept0 = (df_gs %>% filter(google_id==gsid))$dept
+    if (dept0 == "BIO") {
+      return(0)
+    } else if (dept0 == "CS") {
+      return(1)
+    } else {
+      return(2)
+    }
+  }
 }
 
-f_get_xdf <- function (gsid) {
-  xd = (df_gs %>% filter(google_id==gsid))$XDIndicator
-  return(as.character(xd))
-}
-
-f_get_xdp <- function (coauth) {
+f_get_iXDp <- function (coauth) {
   coauth = unlist(strsplit(as.character(coauth), ","))
   n = length(coauth)
   if (n > 1) {
@@ -80,15 +79,26 @@ f_get_xdp <- function (coauth) {
   return(0)
 }
 
+f_get_pagerank <- function(gsid) {
+  return((df_gs %>% filter(google_id==gsid))$PRCentrality)
+}
+
+f_get_bridgefrac <- function(coauth) {
+  coauth = unlist(strsplit(as.character(coauth), ","))
+  poli = coauth [coauth %in% c(0:2)]
+  frac = length(which(poli==2))/length(poli)
+  return(frac)
+}
+
 # Main
+#df$XDF = sapply(df$google_id, f_get_xdf)
+#df$coauthor_codes = sapply(df$coauthor_codes, f_remove_pollinators)
 df$zp = mapply(f_get_normcite, df$google_id, df$year, df$citations)
-df$ap = sapply(df$coauthor_codes, f_get_totalcoauth) #with pollinators ?
+df$ap = sapply(df$coauthor_codes, f_get_totalcoauth)
 df$tp = mapply(f_get_careerage, df$google_id, df$year)
+df$iXDp = sapply(df$coauthor_codes, f_get_iXDp)
 df$PR = sapply(df$google_id, f_get_pagerank)
 df$bf = sapply(df$coauthor_codes, f_get_bridgefrac)
-df$coauthor_codes = sapply(df$coauthor_codes, f_remove_pollinators)
-df$XDF = sapply(df$google_id, f_get_xdf)
-df$XDP = sapply(df$coauthor_codes, f_get_xdp)
 
 # Write data
 fn = paste0("Panel_Analysis_Data.csv")
