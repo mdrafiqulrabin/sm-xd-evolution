42ac965d119101adb06487eb05b65c2db93948b7
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-22 19:19:20 -0500

    f_get_bridgelambda

--- Data/Panel_Analysis/Panel_Analysis.R
+++ Data/Panel_Analysis/Panel_Analysis.R
@@ -3,10 +3,12 @@ setwd("~/Workspace/RStudio/sm-xd-evolution/Data/Panel_Analysis/")
 
 # Import library
 library(readr, warn.conflicts=F)
+library(dplyr, warn.conflicts=F)
 
 # Load data
 df_ps = read.csv("../GoogleScholar_paper_stats.csv") # paper stats
 df_gs = read.csv("../Faculty_GoogleScholar_Funding_Data_N4190.csv") # google scholars
+df_lm = read.csv("../Faculty_Googlescholar_lambda_mu_N4190.csv") # lambda mu
 df_bs = read.csv("../Biology_citations_stats_CitationNormalizationData.csv")
 df_cs = read.csv("../ComputerScience_citations_stats_CitationNormalizationData.csv")
 df = df_ps # new df for panel model
@@ -83,14 +85,11 @@ f_get_pagerank <- function(gsid) {
   return((df_gs %>% filter(google_id==gsid))$PRCentrality)
 }
 
-f_get_bridgefrac <- function(coauth) {
-  coauth = unlist(strsplit(as.character(coauth), ","))
-  poli = coauth [coauth %in% c(0:2)]
-  frac = length(which(poli==2))/length(poli)
-  return(frac)
+f_get_bridgelambda <- function(gsid) {
+  return((df_lm %>% filter(google_id==gsid))$Lambda)
 }
 
-df = read.csv("Panel_Analysis_Data.csv")
+# df = read.csv("Panel_Analysis_Data.csv")
 
 # Main
 #df$coauthor_codes = sapply(df$coauthor_codes, f_remove_pollinators)
@@ -100,8 +99,8 @@ df$zp = mapply(f_get_normcite, df$google_id, df$year, df$citations)
 df$ap = sapply(df$coauthor_codes, f_get_totalcoauth)
 df$tp = mapply(f_get_careerage, df$google_id, df$year)
 df$iXDp = sapply(df$coauthor_codes, f_get_iXDp)
-df$PR = sapply(df$google_id, f_get_pagerank)
-df$bf = sapply(df$coauthor_codes, f_get_bridgefrac)
+df$PRCentrality = sapply(df$google_id, f_get_pagerank)
+df$Lambda = sapply(df$google_id, f_get_bridgelambda)
 
 # Write data
 fn = paste0("Panel_Analysis_Data.csv")
