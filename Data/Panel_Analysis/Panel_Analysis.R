2facbdff7fff15692ce3b5c85c61092b12c255f3
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-26 01:38:40 -0500

    iXDp without pollinator

--- Data/Panel_Analysis/Panel_Analysis.R
+++ Data/Panel_Analysis/Panel_Analysis.R
@@ -58,7 +58,7 @@ f_get_normcite <- function (gsid, pyr, ncite) {
 f_get_totalcoauth <- function (coauth) {
   coauth = unlist(strsplit(as.character(coauth), ","))
   n = length(coauth)
-  return(n)
+  return(n-1)
 }
 
 f_get_careerage <- function (gsid, pubyr) {
@@ -89,10 +89,10 @@ f_get_bridgelambda <- function(gsid) {
   return((df_lm %>% filter(google_id==gsid))$Lambda)
 }
 
-# df = read.csv("Panel_Analysis_Data.csv")
+# df = read.csv("Panel_Analysis_Data.csv") #bypass
 
 # Main
-#df$coauthor_codes = sapply(df$coauthor_codes, f_remove_pollinators)
+df$coauthor_codes = sapply(df$coauthor_codes, f_remove_pollinators)
 df$XDIndicator = sapply(df$google_id, f_get_xd)
 df$dept = sapply(df$google_id, f_get_dept)
 df$zp = mapply(f_get_normcite, df$google_id, df$year, df$citations)
