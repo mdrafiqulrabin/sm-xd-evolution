cc650e159bbc4b76ff3442d183d4dcb5fecdc84b
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-26 02:03:05 -0500

    TableS4: model1

--- TableS4.R
+++ TableS4.R
@@ -11,11 +11,14 @@ df = df[df$year >= 1970,]
 df = df[df$year <= 2017,]
 df = filter(df, df$PRCentrality > 0)
 
-df['ap'] = log1p(df['ap'])
-df['PRCentrality'] = log(df['PRCentrality'])
-
-mod1 = lm(zp ~ ap + tp + iXDp + PRCentrality +  factor(year), data=df)
+# Model (1) No Fixed Effects
+df1 = df
+df1['ap'] = log1p(df1['ap'])
+df1['PRCentrality'] = log(df1['PRCentrality'])
+mod1 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df1)
 summary(mod1)
 
-mod3 = lm(zp ~ ap + tp + iXDp + factor(year), data=df)
+# Model (3) Fixed Effects
+df3 = df
+mod3 = lm(zp ~ ap + tp + iXDp + factor(year), data=df3)
 summary(mod3)
