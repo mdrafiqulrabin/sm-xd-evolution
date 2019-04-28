713ec8b37450124575a31a0cbc8ddb96ba51646c
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-04-28 03:03:56 -0500

    TableS4: Log Transformation and  Std

--- TableS4.R
+++ TableS4.R
@@ -11,21 +11,22 @@ df = df[df$year >= 1970,]
 df = df[df$year <= 2017,]
 df = filter(df, df$PRCentrality > 0) #3900 connected scholars
 
+# Log Transformation
+df['ap'] = log(df['ap'])
+df['PRCentrality'] = log(df['PRCentrality'])
+df['Lambda'] = log(df['Lambda'])
+
 nrow(df) #n=413565
 
 # Model (1) No Fixed Effects
 df1 = df
-df1['ap'] = log1p(df1['ap'])
-df1['PRCentrality'] = log(df1['PRCentrality'])
 mod1 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df1)
 summary(mod1)
 
 # Model (2) No Fixed Effects [Std]
 df2 = df
-df2$ap = df2$ap + 1
-df2['ap'] = log(df2['ap'])
-df2['PRCentrality'] = log(df2['PRCentrality'])
-df2[,8:12] <- data.frame(sapply(df2[8:12], scale), stringsAsFactors=F)
+df2[,8:9] <- data.frame(sapply(df2[8:9], scale), stringsAsFactors=F)
+df2[,11:12] <- data.frame(sapply(df2[11:12], scale), stringsAsFactors=F)
 mod2 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df2)
 summary(mod2)
 
@@ -36,7 +37,6 @@ summary(mod3)
 
 # Model (4) Fixed Effects [Std]
 df4 = df
-df4['ap'] = log1p(df4['ap'])
-df2[,8:10] <- data.frame(sapply(df2[8:10], scale), stringsAsFactors=F)
+df2[,8:9] <- data.frame(sapply(df2[8:9], scale), stringsAsFactors=F)
 mod4 = lm(zp ~ ap + tp + iXDp + factor(year), data=df4)
 summary(mod4)
