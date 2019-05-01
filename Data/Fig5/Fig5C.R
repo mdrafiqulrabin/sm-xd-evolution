857eecbe8730b93563d4b055c7b87eea3d2bb4fa
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-05-01 02:28:44 -0500

    Fig5: git conflict push

---
+++ Data/Fig5/Fig5C.R
@@ -0,0 +1,26 @@
+# Set working directory
+setwd("~/Workspace/RStudio/sm-xd-evolution/Data/Fig5/")
+
+# Import library
+library(readr, warn.conflicts=F)
+library(dplyr, warn.conflicts=F)
+
+# Read CSV
+df_gs = read.csv("../Faculty_GoogleScholar_Funding_Data_N4190.csv") # google scholars
+gsids = as.vector(df_gs$google_id)
+df_pa = read.csv("../Panel_Analysis/Panel_Analysis_Data.csv", stringsAsFactors=F)
+length(unique(df_pa$google_id))
+
+f_get_beta_i_all <- function(x) {
+  gs1k = sample(gsids, 1000)
+  df_5c = df_pa[df_pa$google_id %in% gs1k,]
+  mod_5c = lm(zp ~ ap + tp + iXDp + factor(year), data=df_5c)
+  return((mod_5c$coefficients)[4][[1]])
+}
+
+beta_i_all = replicate(n = 1000, expr = f_get_beta_i_all(x))
+fn = paste0("beta_i_all.csv")
+if (file.exists(fn)) file.remove(fn)
+write.csv(beta_i_all, file = fn, row.names=F)
+
+print("Done")
