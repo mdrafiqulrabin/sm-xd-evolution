1b2564c959dda13ec2684860310e306774750282
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-05-01 02:34:31 -0500

    Fig5C: scale and axis

--- Fig5C.R
+++ Fig5C.R
@@ -9,6 +9,14 @@ library(dplyr, warn.conflicts=F)
 df = read.csv("beta_i_all.csv")
 
 # Histogram
-hg = ggplot(data=df, aes(x=df$x, y=..count..)) +
-  geom_histogram(binwidth = 0.006, colour = 'black', alpha=0.5)
+hg = ggplot(data=df, aes(x=df$x, y=(..count..)/1000)) +
+  geom_histogram(binwidth = 0.006, colour = 'black', alpha=0.5) +
+  scale_x_continuous(breaks = seq(-0.05,0.20,0.05)) +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0.0,0.12,0.04)) +
+  coord_cartesian(xlim = c(-0.11, 0.22), ylim = c(0.0, 0.15), clip = 'off') +
+  xlab(expression(paste("Cross-disciplinary coefficient, ", beta[I], "", sep=""))) + 
+  ylab(expression(paste("Prob. dist. ", P(beta[I]),"", sep=""))) +
+  theme(panel.background = element_blank(), 
+        panel.border = element_rect(color = "black", fill = NA, size=0.5),
+        plot.title = element_text(hjust = 0.5)) 
 hg
