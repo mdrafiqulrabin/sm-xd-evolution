177e40d2b4270c6cd21dfeebaabc4b14b632410c
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-05-01 03:05:11 -0500

    Fig5D: beta_i for xd F

--- Data/Fig5/Fig5D.R
+++ Data/Fig5/Fig5D.R
@@ -9,18 +9,19 @@ library(dplyr, warn.conflicts=F)
 df_gs = read.csv("../Faculty_GoogleScholar_Funding_Data_N4190.csv") # google scholars
 gsids = as.vector(df_gs$google_id)
 df_pa = read.csv("../Panel_Analysis/Panel_Analysis_Data.csv", stringsAsFactors=F)
-length(unique(df_pa$google_id)) # F(all) = 4190
+df_pa = df_pa[df_pa$XDIndicator=="XD",] # Only XD faculty
+length(unique(df_pa$google_id)) # F(xd) = 1247
 
-f_get_beta_i_all <- function(x) {
+f_get_beta_i_xd <- function(x) {
   gs1k = sample(gsids, 1000)
-  df_5c = df_pa[df_pa$google_id %in% gs1k,]
-  mod_5c = lm(zp ~ ap + tp + iXDp + factor(year), data=df_5c)
-  return((mod_5c$coefficients)[4][[1]])
+  df_5d = df_pa[df_pa$google_id %in% gs1k,]
+  mod_5d = lm(zp ~ ap + tp + iXDp + factor(year), data=df_5d)
+  return((mod_5d$coefficients)[4][[1]])
 }
 
-beta_i_all = replicate(n = 1000, expr = f_get_beta_i_all(x))
-fn = paste0("beta_i_all.csv")
+beta_i_xd = replicate(n = 1000, expr = f_get_beta_i_xd(x))
+fn = paste0("beta_i_xd.csv")
 if (file.exists(fn)) file.remove(fn)
-write.csv(beta_i_all, file = fn, row.names=F)
+write.csv(beta_i_xd, file = fn, row.names=F)
 
 print("Done")
