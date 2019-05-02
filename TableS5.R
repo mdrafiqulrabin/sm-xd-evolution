6e33611aab397d2ba3d6ee6a89cbc4c9758f2b1c
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-05-02 14:09:36 -0500

    TableS5: Fix no-fixed and fixed effect.

--- TableS5.R
+++ TableS5.R
@@ -4,6 +4,9 @@ setwd("~/Workspace/RStudio/sm-xd-evolution/Data/Panel_Analysis/")
 # Import library
 library(readr, warn.conflicts=F)
 library(dplyr, warn.conflicts=F)
+library(Hmisc, warn.conflicts=F)
+library(miceadds, warn.conflicts=F)
+library(multiwayvcov, warn.conflicts=F)
 
 # Load data and filter
 df = read.csv("Panel_Analysis_Data.csv",stringsAsFactors=F)
@@ -21,27 +24,35 @@ df['Lambda'] = log(df['Lambda'])
 
 # Model (1) No Fixed Effects
 df1 = df
-mod1 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df1)
+#mod1 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df1)
+mod1 = lm.cluster(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df1, cluster="google_id")
 summary(mod1)
 
 # Model (2) No Fixed Effects [Std]
 df2 = df
 df2[,8:9] <- data.frame(sapply(df2[8:9], scale), stringsAsFactors=F)
 df2[,11:12] <- data.frame(sapply(df2[11:12], scale), stringsAsFactors=F)
-mod2 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df2)
+#mod2 = lm.(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df2)
+mod2 = lm.cluster(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df2, cluster="google_id")
 summary(mod2)
 
 # Model (3) Fixed Effects
 df3 = df
-mod3 = lm(zp ~ ap + tp + iXDp + factor(year), data=df3)
-summary(mod3) #iXDp=0.1458400
+#mod3 = lm(zp ~ ap + tp + iXDp + factor(year), data=df3)
+#summary(mod3)
+mod3 = plm(zp ~ ap + tp + iXDp + factor(year), data=df3, index="google_id", model="within")
+coeftest(mod3, vcov = vcovHC, type = "HC0")  #iXDp=0.1123566
+within_intercept(mod3, vcov = vcovHC, type = "HC0")
 # 95% CI
 ci95 = confint(mod3, "iXDp", level=0.95)
 c (ci95[1], ci95[2])
-#[1] 0.1133847 0.1782953
+#[1] 0.08043266 0.14428053
 
 # Model (4) Fixed Effects [Std]
 df4 = df
 df4[,8:9] <- data.frame(sapply(df4[8:9], scale), stringsAsFactors=F)
-mod4 = lm(zp ~ ap + tp + iXDp + factor(year), data=df4)
-summary(mod4)
+#mod4 = lm(zp ~ ap + tp + iXDp + factor(year), data=df4)
+#summary(mod4)
+mod4 = plm(zp ~ ap + tp + iXDp + factor(year), data=df4, index="google_id", model="within")
+coeftest(mod4, vcov = vcovHC, type = "HC0")
+within_intercept(mod4, vcov = vcovHC, type = "HC0")
