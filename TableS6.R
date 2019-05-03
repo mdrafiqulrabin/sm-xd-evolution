32c1c5c12b4530bc35d88f8bbdf2e61764209cb9
salahuddin975 <salahuddin975@gmail.com>
2019-05-02 21:20:36 -0500

    Update for tableS6

--- TableS6.R
+++ TableS6.R
@@ -16,7 +16,7 @@ nrow(df)   # 166621
 
 # 2SF6AXQAAAAJ
 
-# Year and ixdp
+#================== Filter with Year and ixdp ===================
 
 all_gs = unique(df$google_id) 
 new_df = df[df$google_id=="ds", ]
@@ -63,12 +63,8 @@ if (file.exists(fn)) file.remove(fn)
 write.csv(new_df, file = fn, row.names=F)
 
 
-#===================== year and ap
+#===================== Filter with year and ap ===================
 
-
-# -chjN0MAAAAJ
-# -IMW-w0AAAAJ
-# 7BM1uyYAAAAJ
 
 df_ixdp = read.csv("tables6_year_ixdp.csv", header = T)
 nrow(df_ixdp)
@@ -80,7 +76,7 @@ matched = df_ixdp[df_ixdp$google_id=="ds", ]
 count =0
 for (gs in all_gs) 
 {
-  print(gs)
+#  print(gs)
   pubs = df_ixdp[df_ixdp$google_id == gs,]
   pubs <- pubs[order(pubs$year),] 
   nrow(pubs)
@@ -99,13 +95,9 @@ for (gs in all_gs)
     {
       row1 = pubs_year[i,]
       j = i + 1
-      while(j <= num_row)
+      while(j < num_row)
       {
         row2 = pubs_year[j, ]
-#        print(row1)
-#        print(row1[8])
-#        print("-")
-#        print(row2)
         prcnt = ((abs(row1[8] - row2[8])) / ((row1[8] + row2[8])/2)) * 100
 
         if(prcnt <= 20)
@@ -118,68 +110,45 @@ for (gs in all_gs)
         j = j+ 1
       }
     }
-
   }
   #  print(pubs$google_id)
-#  print(count)
+  print(count)
   count = count + 1
 }
 
-View(matched)
+nrow(matched)
 matched = matched[!duplicated(matched), ]
 
+nrow(matched)
+View(matched)
+
+fn = paste0("tables6_year_ixdp_percent.csv")
+if (file.exists(fn)) file.remove(fn)
+write.csv(matched, file = fn, row.names=F)
 
 
-#-----------------
-tot= 0
-all_gs = unique(new_df$google_id) 
+#=================== Filter by at least 10 match ================
 
-new_df = df[df$google_id=="ds", ]
+matched_final = matched
 
-for (gs in all_gs) {
-  pubs = df[df$google_id == gs,]
-  pubs <- pubs[order(pubs$year),] 
-
-  count = 0
-  for(i in 1:(nrow(pubs)-1)){
-    j=1
-    while(i+j<=nrow(pubs)){
-      diff = pubs[i+j, 2] - pubs[i, 2] 
-      if(diff>=2){
-        break
-      }
-      prcnt = ((abs(pubs[i+j, 8] - pubs[i, 8])) / ((pubs[i+j, 8] + pubs[i, 8])/2)) * 100
-      
-      if(diff < 2 && xor==1 && prcnt <=20)
-      {
-        count = count +1
-      }
-      if(count>=10){
-        break
-      }
-      j=j+1
-    }
-    if(count>=10){
-      break
-    }
-  }
-
-  if(count>=10){
-    print(pubs[1,1])  
-    tot = tot + 1
-    print(tot)
-    new_df = rbind(new_df, pubs)
-  }
-  else {
-    df = df[df$google_id!=gs,]
+all_gs = unique(matched_all$google_id) 
+count =0
+for (gs in all_gs) 
+{
+  pubs = matched_final[matched_final$google_id == gs,]
+  if( nrow(pubs) < 10)
+  {
+    matched_final = matched_final[(matched_final$google_id != gs),]
   }
 }
 
-print(tot)
-nrow(df) 
-nrow(new_df)
-all_gs = unique(df$google_id)
-View(df)
+fn = paste0("tableS6.csv")
+if (file.exists(fn)) file.remove(fn)
+write.csv(matched_final, file = fn, row.names=F)
+
+#===================================
+
+df = matched_final
 
 # Log Transformation
 df['ap'] = log(df['ap'])
@@ -188,25 +157,35 @@ df['Lambda'] = log(df['Lambda'])
 
 # Model (1) No Fixed Effects
 df1 = df
-mod1 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df1)
+#mod1 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df1)
+mod1 = lm.cluster(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df1, cluster="google_id")
 summary(mod1)
 
 # Model (2) No Fixed Effects [Std]
 df2 = df
 df2[,8:9] <- data.frame(sapply(df2[8:9], scale), stringsAsFactors=F)
 df2[,11:12] <- data.frame(sapply(df2[11:12], scale), stringsAsFactors=F)
-mod2 = lm(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df2)
+#mod2 = lm.(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df2)
+mod2 = lm.cluster(zp ~ ap + tp + iXDp + PRCentrality + Lambda + dept + factor(year), data=df2, cluster="google_id")
 summary(mod2)
 
 # Model (3) Fixed Effects
 df3 = df
-mod3 = lm(zp ~ ap + tp + iXDp + factor(year), data=df3)
-summary(mod3)
+#mod3 = lm(zp ~ ap + tp + iXDp + factor(year), data=df3)
+#summary(mod3)
+mod3 = plm(zp ~ ap + tp + iXDp + factor(year), data=df3, index="google_id", model="within")
+coeftest(mod3, vcov = vcovHC, type = "HC0")  #iXDp=0.1123566
+within_intercept(mod3, vcov = vcovHC, type = "HC0")
 # 95% CI
-confint(mod3, "iXDp", level=0.95)
+ci95 = confint(mod3, "iXDp", level=0.95)
+c (ci95[1], ci95[2])
+#[1] 0.08043266 0.14428053
 
 # Model (4) Fixed Effects [Std]
 df4 = df
 df4[,8:9] <- data.frame(sapply(df4[8:9], scale), stringsAsFactors=F)
-mod4 = lm(zp ~ ap + tp + iXDp + factor(year), data=df4)
-summary(mod4)
+#mod4 = lm(zp ~ ap + tp + iXDp + factor(year), data=df4)
+#summary(mod4)
+mod4 = plm(zp ~ ap + tp + iXDp + factor(year), data=df4, index="google_id", model="within")
+coeftest(mod4, vcov = vcovHC, type = "HC0")
+within_intercept(mod4, vcov = vcovHC, type = "HC0")
